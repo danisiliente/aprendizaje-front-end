4f492ab4f18ff1885bb6e6e427873ea0b134e2e5
daniel <danisiliente@gmail.com>
2023-09-04 15:12:55 -0500

    Clase - servicios

---
+++ hotel/js/sistema.js
@@ -0,0 +1,14 @@
+function seleccionar(link){
+
+    let links = document.querySelectorAll;
+
+    links[0] = className="";
+    links[1] = className="";
+    links[2] = className="";
+    links[3] = className="";
+    links[4] = className="";
+    links[5] = className="";
+
+    link = className="selected";
+
+}
