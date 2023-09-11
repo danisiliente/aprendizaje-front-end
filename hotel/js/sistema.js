bc2fa3aa94a06681882a6a669c5f536dbc159a5d
daniel <danisiliente@gmail.com>
2023-09-11 17:07:31 -0500

    Formulario ingreso valida datos

--- hotel/js/sistema.js
+++ hotel/js/sistema.js
@@ -11,3 +11,28 @@ function seleccionar(link){
 
     link.className="selected";
 }
+
+// INICIAR SESION
+ 
+const usuariosValidos = [ //Simula usuarios reales para hacer una validación
+    { nombre: "daniel", contrasenia: "12345" },
+    { nombre: "alejandro", contrasenia: "abcde" },
+    { nombre: "messi", contrasenia: "ronaldo" },
+  ];
+  
+  document.getElementById('validarButton').addEventListener('click', function () {
+    const nombreInput = document.getElementById('nombreInput').value.trim();
+    const contraseniaInput = document.getElementById('contraseniaInput').value.trim();
+  
+    // Buscar el usuario ingresado en la lista de usuarios válidos
+    const usuarioValido = usuariosValidos.find(usuario => usuario.nombre === nombreInput && usuario.contrasenia === contraseniaInput);
+  
+    if (usuarioValido) {
+      alert('¡Usuario válido! Acceso permitido.');
+    } else {
+      alert('Usuario no válido o contraseña incorrecta. Acceso denegado.');
+    }
+  });
+
+//REGISTRARSE
+
